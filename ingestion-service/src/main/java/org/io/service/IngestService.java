266ceb7dd8f7e5d0413894817c9af6f436a24ff1
zaerbib <ilunga.joel@yahoo.fr>
2024-02-21 00:50:56 +0100

    ingest verticle done

--- ingestion-service/src/main/java/org/io/service/IngestService.java
+++ ingestion-service/src/main/java/org/io/service/IngestService.java
@@ -1,20 +1,104 @@
 package org.io.service;
 
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Flowable;
+import io.vertx.amqp.AmqpClientOptions;
+import io.vertx.amqp.AmqpReceiverOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava3.amqp.AmqpClient;
+import io.vertx.rxjava3.amqp.AmqpMessage;
+import io.vertx.rxjava3.amqp.AmqpReceiver;
+import io.vertx.rxjava3.core.AbstractVerticle;
+import io.vertx.rxjava3.core.RxHelper;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.ext.web.Router;
+import io.vertx.rxjava3.ext.web.RoutingContext;
+import io.vertx.rxjava3.ext.web.handler.BodyHandler;
 import io.vertx.rxjava3.kafka.client.producer.KafkaProducer;
 import io.vertx.rxjava3.kafka.client.producer.KafkaProducerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Hello world!
  */
-public class IngestService {
+public class IngestService extends AbstractVerticle {
 
   private static final int HTTP_PORT = 3002;
   private static final Logger logger = LoggerFactory.getLogger(IngestService.class);
 
   private KafkaProducer<String, JsonObject> updateProducer;
+
+  @Override
+  public Completable rxStart() {
+    updateProducer = KafkaProducer.create(vertx, kafkaConfig());
+
+    AmqpClientOptions amqpClientOptions = amqpConfig();
+    AmqpReceiverOptions receiverOptions = new AmqpReceiverOptions().setAutoAcknowledgement(false).setDurable(true);
+
+    AmqpClient.create(vertx, amqpClientOptions).rxConnect().flatMap(conn -> conn.rxCreateReceiver("step-events", receiverOptions)).flatMapPublisher(AmqpReceiver::toFlowable).doOnError(this::logAmqpError).retryWhen(this::retryLater).subscribe(this::handleAmqpMessage);
+
+    Router router = Router.router(vertx);
+    router.post().handler(BodyHandler.create());
+    router.post("/ingest").handler(this::httpIngest);
+
+    return vertx.createHttpServer().requestHandler(router).rxListen(HTTP_PORT).ignoreElement();
+  }
+
+  private Flowable<Throwable> retryLater(Flowable<Throwable> errs) {
+    return errs.delay(10, TimeUnit.SECONDS, RxHelper.scheduler(vertx));
+  }
+
+  private AmqpClientOptions amqpConfig() {
+    return new AmqpClientOptions().setHost("localhost").setPort(5672).setUsername("artemis").setPassword("simetraehcapa");
+  }
+
+  private Map<String, String> kafkaConfig() {
+    Map<String, String> config = new HashMap<>();
+    config.put("boostrap.servers", "stepk_kafka_1:9092");
+    config.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    config.put("value.serializer", "io.vertx.kafka.client.serialization.JsonObjectSerializer");
+    config.put("acks", "1");
+
+    return config;
+  }
+
+  private void logAmqpError(Throwable err) {
+    logger.error("Woops AMQP", err);
+  }
+
+  private void handleAmqpMessage(AmqpMessage message) {
+    if (!"application/json".equals(message.contentType()) || invalidIngestedJson(message.bodyAsJsonObject())) {
+      logger.error("Invalid AMQP message (discarded): {}", message.bodyAsBinary());
+      message.accepted();
+      return;
+    }
+
+    JsonObject payload = message.bodyAsJsonObject();
+    KafkaProducerRecord<String, JsonObject> record = makeKafkaRecord(payload);
+    updateProducer.rxSend(record).subscribe(ok -> message.accepted(), err -> {
+      logger.error("AMQP ingestion faled", err);
+    });
+  }
+
+  private void httpIngest(RoutingContext ctx) {
+    JsonObject payload = ctx.body().asJsonObject();
+    if (invalidIngestedJson(payload)) {
+      logger.error("Invalid HTTP JSON (discarded): {}", payload.encodePrettily());
+      ctx.fail(400);
+      return;
+    }
+
+    KafkaProducerRecord<String, JsonObject> record = makeKafkaRecord(payload);
+    updateProducer.rxSend(record).doOnSuccess(item -> ctx.response().end()).doOnError(err -> {
+      logger.error("HTTP ingestion failed", err);
+      ctx.fail(500);
+    }).subscribe();
+  }
 
   private boolean invalidIngestedJson(JsonObject payload) {
     return !payload.containsKey("deviceId")
@@ -24,15 +108,16 @@ public class IngestService {
 
   private KafkaProducerRecord<String, JsonObject> makeKafkaRecord(JsonObject payload) {
     String deviceId = payload.getString("deviceId");
-    JsonObject recordData = new JsonObject()
-      .put("deviceId", payload.getString("deviceId"))
-      .put("deviceSync", payload.getString("deviceSync"))
-      .put("stepsCount", payload.getString("stepsCount"));
+    JsonObject recordData = new JsonObject().put("deviceId", payload.getString("deviceId")).put("deviceSync", payload.getString("deviceSync")).put("stepsCount", payload.getString("stepsCount"));
 
     return KafkaProducerRecord.create("incoming.steps", deviceId, recordData);
   }
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
+    Vertx vertx = Vertx.vertx();
+    vertx.rxDeployVerticle(new IngestService())
+      .doOnSuccess(ok -> logger.info("HTTP server started on port {}", HTTP_PORT))
+      .doOnError(err -> logger.error("Woops", err)).subscribe();
   }
 }
--- ingestion-service/src/test/java/org/io/service/IngestServiceTest.java
+++ ingestion-service/src/test/java/org/io/service/IngestServiceTest.java
@@ -1,38 +1,191 @@
 package org.io.service;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import io.vertx.amqp.AmqpClientOptions;
+import io.vertx.core.json.JsonObject;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
+import io.vertx.rxjava3.amqp.AmqpClient;
+import io.vertx.rxjava3.amqp.AmqpMessage;
+import io.vertx.rxjava3.core.RxHelper;
+import io.vertx.rxjava3.core.Vertx;
+import io.vertx.rxjava3.kafka.admin.KafkaAdminClient;
+import io.vertx.rxjava3.kafka.client.consumer.KafkaConsumer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+import static io.restassured.RestAssured.given;
 
 /**
  * Unit test for simple App.
  */
-public class IngestServiceTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public IngestServiceTest(String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( IngestServiceTest.class );
-    }
+@ExtendWith(VertxExtension.class)
+@Testcontainers
+public class IngestServiceTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  private static final Logger logger = LoggerFactory.getLogger(IngestServiceTest.class);
+
+  @Container
+  private static final DockerComposeContainer CONTAINERS =
+    new DockerComposeContainer(new File("../docker-compose.yml"))
+      .withExposedService("artemis_1", 5672)
+      .withExposedService("kafka_1", 9092);
+
+  private static RequestSpecification requestSpecification;
+  private static KafkaConsumer<String, JsonObject> kafkaConsumer;
+  private AmqpClient amqpClient;
+
+  @BeforeAll
+  public static void prepareSpec() {
+    requestSpecification = new RequestSpecBuilder()
+      .addFilters(asList(new ResponseLoggingFilter(), new ResponseLoggingFilter()))
+      .setBaseUri("http://localhost:3002")
+      .build();
+  }
+
+  public static Map<String, String> kafkaConfig() {
+    Map<String, String> config = new HashMap<>();
+    config.put("bootstrap.servers", "localhost:9092");
+    config.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+    config.put("value.deserializer", "io.vertx.kafka.client.serialization.JsonObjectDeserializer");
+    config.put("auto.offset.reset", "earliest");
+    config.put("enable.auto.commit", "false");
+    config.put("group.id", "ingester-test-" + System.currentTimeMillis());
+    return config;
+  }
+
+  public static AmqpClientOptions amqpClientOptions() {
+    return new AmqpClientOptions()
+      .setHost("localhost")
+      .setPort(5672)
+      .setUsername("artemis")
+      .setPassword("simetraehcapa");
+  }
+
+  @BeforeEach
+  public void setup(Vertx vertx, VertxTestContext testContext) {
+    kafkaConsumer = KafkaConsumer.create(vertx, kafkaConfig());
+    amqpClient = AmqpClient.create(vertx, amqpClientOptions());
+    KafkaAdminClient adminClient = KafkaAdminClient.create(vertx, kafkaConfig());
+
+    vertx.rxDeployVerticle(new IngestService())
+      .delay(1000, TimeUnit.MILLISECONDS, RxHelper.scheduler(vertx))
+      .flatMapCompletable(id -> adminClient.rxDeleteTopics(Collections.singletonList("incoming.steps")))
+      .onErrorComplete()
+      .doOnComplete(testContext::completeNow)
+      .doOnError(testContext::failNow)
+      .subscribe();
+  }
+
+  @Test
+  @DisplayName("Ingest well-formed AMQP Message")
+  void amqIngest(VertxTestContext testContext) {
+    JsonObject body = new JsonObject()
+      .put("deviceId", "123")
+      .put("deviceSync", 1L)
+      .put("stepsCount", 500);
+
+    amqpClient.rxConnect()
+      .flatMap(con -> con.rxCreateSender("step-events"))
+      .doOnSuccess(sender -> {
+        AmqpMessage message = AmqpMessage.create()
+          .durable(true)
+          .ttl(5000)
+          .withJsonObjectAsBody(body).build();
+        sender.send(message);
+        testContext.completeNow();
+      })
+      .doOnError(testContext::failNow)
+      .subscribe();
+
+    kafkaConsumer.rxSubscribe("incoming.steps");
+    kafkaConsumer.handler(record -> testContext.verify(() -> {
+      assertThat(record.key()).isEqualTo("123");
+      JsonObject json = record.value();
+      assertThat(json.getString("deviceId")).isEqualTo("123");
+      assertThat(json.getLong("deviceSync")).isEqualTo(1L);
+      assertThat(json.getInteger("stepsCount")).isEqualTo(500);
+      testContext.completeNow();
+    }));
+  }
+
+  @Test
+  @DisplayName("Ingest a badly-formed AMQP message and observe no kafka record")
+  void amqIngestWrong(Vertx vertx, VertxTestContext testContext) {
+    JsonObject body = new JsonObject();
+
+    amqpClient.rxConnect()
+      .flatMap(connection -> connection.rxCreateSender("step-events"))
+      .doOnSuccess(sender -> {
+        AmqpMessage message = AmqpMessage.create()
+          .durable(true)
+          .ttl(5000)
+          .withJsonObjectAsBody(body).build();
+        sender.send(message);
+        testContext.completeNow();
+      })
+      .doOnError(testContext::failNow)
+      .subscribe();
+
+    kafkaConsumer.subscribe("incoming.steps")
+      .toFlowable()
+      .timeout(3, TimeUnit.SECONDS, RxHelper.scheduler(vertx))
+      .subscribe(
+        record -> testContext.failNow(new IllegalStateException("We must not get a record")),
+        err -> {
+          if (err instanceof TimeoutException) {
+            testContext.completeNow();
+          } else {
+            testContext.failNow(err);
+          }
+        });
+  }
+
+  @Test
+  @DisplayName("Ingest a well-formed JSON data over HTTP")
+  void httpIngest(VertxTestContext testContext) {
+    JsonObject body = new JsonObject()
+      .put("deviceId", "456")
+      .put("deviceSync", 3L)
+      .put("stepsCount", 125);
+
+    given(requestSpecification)
+      .contentType(ContentType.JSON)
+      .body(body.encode())
+      .post("/ingest")
+      .then()
+      .assertThat()
+      .statusCode(200);
+
+    kafkaConsumer.subscribe("incoming.steps");
+    kafkaConsumer.handler(record -> testContext.verify(() -> {
+      assertThat(record.key()).isEqualTo("456");
+      JsonObject json = record.value();
+      assertThat(json.getString("deviceId")).isEqualTo("456");
+      assertThat(json.getLong("deviceSync")).isEqualTo(3L);
+      assertThat(json.getInteger("stepsCount")).isEqualTo(125);
+      testContext.completeNow();
+    }));
+  }
 }
