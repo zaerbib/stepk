7eb31926ccb9c96608a4aaa265b2b0ad387aa5dc
zaerbib <ilunga.joel@yahoo.fr>
2024-02-22 22:18:00 +0100

    new application

---
+++ sender/src/main/java/org/kafka/send/sender/SenderObjectApplication.java
@@ -0,0 +1,13 @@
+package org.kafka.send.sender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderObjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderObjectApplication.class, args);
+	}
+
+}
---
+++ sender/src/test/java/org/kafka/send/sender/SenderObjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kafka.send.sender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderObjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
