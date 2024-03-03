3435c0a430a5c039f3381a5f8c8ce6670f96076c
zaerbib <ilunga.joel@yahoo.fr>
2024-03-03 16:47:24 +0100

    update authente service

--- user-profile-service/src/main/java/org/io/service/service/impl/UserProfileServiceApiImpl.java
+++ user-profile-service/src/main/java/org/io/service/service/impl/UserProfileServiceApiImpl.java
@@ -14,6 +14,7 @@ import org.io.service.model.User;
 import org.io.service.service.UserProfileServiceApi;
 
 import java.util.NoSuchElementException;
+import java.util.regex.Pattern;
 
 @Slf4j
 public class UserProfileServiceApiImpl implements UserProfileServiceApi {
@@ -21,6 +22,10 @@ public class UserProfileServiceApiImpl implements UserProfileServiceApi {
   private final MongoAuthentication authProvider;
   private final MongoUserUtil userUtil;
   private final MongoClient mongoClient;
+
+  private final Pattern validUsername = Pattern.compile("\\w[\\w+|-]*");
+  private final Pattern validDeviceId = Pattern.compile("\\w[\\w+|-]*");
+  private final Pattern validEmail = Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
 
   public UserProfileServiceApiImpl(MongoAuthentication authProvider, MongoUserUtil userUtil, MongoClient mongoClient) {
     this.authProvider = authProvider;
@@ -30,11 +35,12 @@ public class UserProfileServiceApiImpl implements UserProfileServiceApi {
 
   @Override
   public void authenticate(User body, ServiceRequest request, Handler<AsyncResult<ServiceResponse>> resultHandler) {
-    authProvider.authenticate(body.toJson()).onSuccess(user ->
-      completeEmptySuccess(user.principal(), resultHandler)
-    ).onFailure(err ->
-      handleAuthenticationError(err, resultHandler)
-    );
+    authProvider.authenticate(fromPojoToJson(body))
+      .onSuccess(user ->
+        completeEmptySuccess(user.principal(), resultHandler)
+      ).onFailure(err ->
+        handleAuthenticationError(err, resultHandler)
+      );
   }
 
   @Override
@@ -57,6 +63,7 @@ public class UserProfileServiceApiImpl implements UserProfileServiceApi {
   @Override
   public void registerUser(User body, ServiceRequest request, Handler<AsyncResult<ServiceResponse>> resultHandler) {
 
+    this.validationRegistration(body, resultHandler);
     JsonObject extraInfo = new JsonObject().put("$set", new JsonObject()
       .put("email", body.getEmail())
       .put("city", body.getCity())
@@ -77,12 +84,17 @@ public class UserProfileServiceApiImpl implements UserProfileServiceApi {
   }
 
   private void completeEmptySuccess(JsonObject user, Handler<AsyncResult<ServiceResponse>> resultHandler) {
-    resultHandler.handle(Future.succeededFuture(ServiceResponse.completedWithJson(user).putHeader("Content-Type", "application/json").setStatusCode(200)));
+    resultHandler.handle(Future.succeededFuture(ServiceResponse.completedWithJson(user)
+      .putHeader("Content-Type", "application/json")
+      .setStatusCode(200)));
   }
 
   private void handleAuthenticationError(Throwable err, Handler<AsyncResult<ServiceResponse>> resultHandler) {
     log.error("Authentication problem {}", err.getMessage());
-    resultHandler.handle(Future.succeededFuture(new ServiceResponse().setStatusCode(401).setStatusMessage("Authentication failed")));
+    resultHandler.handle(Future.succeededFuture(
+      new ServiceResponse()
+        .setStatusCode(401)
+        .setStatusMessage("Authentication failed")));
   }
 
   private void completeFetchRequest(JsonObject user, Handler<AsyncResult<ServiceResponse>> resultHandler) {
@@ -121,12 +133,50 @@ public class UserProfileServiceApiImpl implements UserProfileServiceApi {
     resultHandler.handle(Future.succeededFuture(new ServiceResponse().setStatusCode(200)));
   }
 
-  private void handleRegistrationError(Throwable err, Handler<AsyncResult<ServiceResponse>> resultHandler) {
+  private void handleRegistrationError(Throwable err,
+                                       Handler<AsyncResult<ServiceResponse>> resultHandler) {
     if (isIndexViolated(err)) {
       log.error("Registration failure: {}", err.getMessage());
       resultHandler.handle(Future.succeededFuture(new ServiceResponse().setStatusCode(409)));
     } else {
       fail500(err, resultHandler);
     }
+  }
+
+  private void validationRegistration(User user,
+                                      Handler<AsyncResult<ServiceResponse>> resultHandler) {
+    if (anyRegistrationFieldIsMissing(user) || anyRegistrationFieldIsWrong(user)) {
+      resultHandler.handle(Future.succeededFuture(
+        new ServiceResponse()
+          .setStatusCode(400)
+      ));
+    }
+  }
+
+  private boolean anyRegistrationFieldIsMissing(User user) {
+    JsonObject userObject = fromPojoToJson(user);
+    return !(userObject.containsKey("username") &&
+      userObject.containsKey("password") &&
+      userObject.containsKey("email") &&
+      userObject.containsKey("city") &&
+      userObject.containsKey("deviceId") &&
+      userObject.containsKey("makePublic"));
+  }
+
+  private boolean anyRegistrationFieldIsWrong(User user) {
+    return !validUsername.matcher(user.getUsername()).matches() ||
+      !validEmail.matcher(user.getEmail()).matches() ||
+      user.getPassword().trim().isEmpty() ||
+      !validDeviceId.matcher(user.getDeviceId()).matches();
+  }
+
+  private JsonObject fromPojoToJson(User user) {
+    return new JsonObject()
+      .put("username", user.getUsername())
+      .put("password", user.getPassword())
+      .put("email", user.getEmail())
+      .put("city", user.getCity())
+      .put("deviceId", user.getDeviceId())
+      .put("makePublic", user.getMakePublic());
   }
 }
